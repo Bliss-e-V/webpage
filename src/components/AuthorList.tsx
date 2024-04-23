2ae4153321a3b1bc7dceea5005d2d1aede9dfad7
Jan Tiegges <52353665+jantiegges@users.noreply.github.com>
2024-04-23 21:18:38 +0200

    add img component

--- src/components/AuthorList.tsx
+++ src/components/AuthorList.tsx
@@ -1,4 +1,5 @@
-import type { Author } from "@utils/types";
+import { Image } from 'astro:assets';
+import type { Author } from '@utils/types';
 
 type Props = {
   authors: Author[];
@@ -7,14 +8,16 @@ export default function AuthorList({ authors }: Props) {
   return (
     <div className="flex items-center">
       {authors.map((author: Author) => (
-        <img
-          src={`/authors/${author.toLowerCase().replaceAll(" ", "-")}.jpg`}
+        <Image
+          src={`/authors/${author
+            .toLowerCase()
+            .replaceAll(' ', '-')}.jpg`}
           alt={`Image of ${author}`}
           className="-ml-3 rounded-full w-7 h-7 first:-ml-0"
         />
       ))}
       <p className="px-2 tracking-wider truncate text-md text-secondary">
-        {authors.join(", ")}
+        {authors.join(', ')}
       </p>
     </div>
   );
