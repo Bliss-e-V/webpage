bbf3a995704ae1ef928aba16a9a99a5a168b0e71
optzGuitar <leo.leo11@hotmail.de>
2024-06-05 14:37:53 +0200

    test

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -34,7 +34,7 @@ export const Newsletter = () => {
         }
         {
             !wasSuccessful &&
-            <form onSubmit={(e) => { e.preventDefault(); captcha.current.execute() }}>
+            <form onSubmit={(e) => { e.preventDefault(); console.log("Submitted!!"); captcha.current.execute() }}>
                 <div className="flex flex-col justify-center items-center bg-opacity-70 bg-black z-10 p-10 shadow-black allAroundCustomShadow">
                     <Headline level={2} className="text-center mb-4">Sign up for our newsletter</Headline>
                     <p className="text-center mb-4 text-secondary">
