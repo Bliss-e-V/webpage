a18a56296cc8075463062fc1a6924c00a92a492b
optzGuitar <leo.leo11@hotmail.de>
2024-12-13 10:07:24 +0100

    a

--- src/components/data/members.ts
+++ src/components/data/members.ts
@@ -5,20 +5,11 @@ export class Member {
 }
 
 export const members = [
-    new Member("Leo Pinetzki1", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki2", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki3", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki4", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki5", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki6", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki7", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki8", leoPin, "https://linked.in/leopinetzki"),
-    new Member("Leo Pinetzki9", leoPin, "https://linked.in/leopinetzki"),
-    /*new Member("Lorenz Hufe", "", ""),
-    new Member("Justus Westerhof", "", ""),
-    new Member("Arina Belova", "", ""),
-    new Member("Jonas Loos", "", ""),
-    new Member("Joseph Tschörner", "", ""),
-    new Member("Duc ???", "", ""),
-    new Member("Cederic Assman", "", ""),*/
+    new Member("Justus Westerhoff", leoPin, "https://www.linkedin.com/in/justus-westerhoff/"),
+    new Member("Cederic Aßmann", leoPin, "https://www.linkedin.com/in/cederic-a%C3%9Fmann-41904322b"),
+    new Member("Jonas Loos", leoPin, "https://www.linkedin.com/in/jonas-loos/"),
+    new Member("Joseph Tschörner", leoPin, "https://www.linkedin.com/in/joseph-tsch%C3%B6rner-12818427a/"),
+    new Member("Mia Prokopovych ", leoPin, "https://www.linkedin.com/in/mia-prokopovych-9aaa26302"),
+    new Member("Duc Hoang", leoPin, "https://www.linkedin.com/in/ly-duc-hoang/"),
+    new Member("Leo Pinetzki", leoPin, "https://www.linkedin.com/in/leo-pinetzki/"),
 ]
