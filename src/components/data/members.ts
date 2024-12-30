979e95daa08cc85b1a04a52a342cf2786863f169
optzGuitar <leo.leo11@hotmail.de>
2024-12-30 18:15:43 +0100

    a

--- src/components/data/members.ts
+++ src/components/data/members.ts
@@ -11,19 +11,19 @@ import philipa from "../../images/members/philipa.png"
 import josi from "../../images/members/josi.png"
 
 export class Member {
-    constructor(public name: string, public image: string, public link: string) { }
+    constructor(public name: string, public surname: string, public image: string, public link: string) { }
 }
 
 export const members = [
-    new Member("Justus Westerhoff", justus, "https://www.linkedin.com/in/justus-westerhoff/"),
-    new Member("Cederic Aßmann", cederic, "https://www.linkedin.com/in/cederic-a%C3%9Fmann-41904322b"),
-    new Member("Jonas Loos", jonas, "https://www.linkedin.com/in/jonas-loos/"),
-    new Member("Joseph Tschörner", joseph, "https://www.linkedin.com/in/joseph-tsch%C3%B6rner-12818427a/"),
-    new Member("Mia Prokopovych ", mia, "https://www.linkedin.com/in/mia-prokopovych-9aaa26302"),
-    // new Member("Duc Hoang", duc, "https://www.linkedin.com/in/ly-duc-hoang/"),
-    new Member("Arina Belova", arina, "https://www.linkedin.com/in/a-belova"),
-    new Member("Lorenz Hufe", lorenz, "https://www.linkedin.com/in/lorenz-hufe"),
-    new Member("Philipa Ringe", philipa, "about:blank"),
-    new Member("Josephine Egerer", josi, "www.linkedin.com/in/josephine-egerer"),
-    new Member("Leo Pinetzki", leo, "https://www.linkedin.com/in/leo-pinetzki/"),
+    new Member("Justus", "Westerhoff", justus, "https://www.linkedin.com/in/justus-westerhoff/"),
+    new Member("Cederic", "Aßmann", cederic, "https://www.linkedin.com/in/cederic-a%C3%9Fmann-41904322b"),
+    new Member("Jonas", "Loos", jonas, "https://www.linkedin.com/in/jonas-loos/"),
+    new Member("Joseph", "Tschörner", joseph, "https://www.linkedin.com/in/joseph-tsch%C3%B6rner-12818427a/"),
+    new Member("Mia", "Prokopovych ", mia, "https://www.linkedin.com/in/mia-prokopovych-9aaa26302"),
+    // new Member("Duc", "Hoang", duc, "https://www.linkedin.com/in/ly-duc-hoang/"),
+    new Member("Arina", "Belova", arina, "https://www.linkedin.com/in/a-belova"),
+    new Member("Lorenz", "Hufe", lorenz, "https://www.linkedin.com/in/lorenz-hufe"),
+    new Member("Philipa", "Ringe", philipa, "about:blank"),
+    new Member("Josephine", "Egerer", josi, "www.linkedin.com/in/josephine-egerer"),
+    new Member("Leo", "Pinetzki", leo, "https://www.linkedin.com/in/leo-pinetzki/"),
 ]
