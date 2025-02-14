f69a2de6a888e80b8c9bc356fa8ec9bb2f214a5a
bmarvin <beesemarvin@gmail.com>
2025-02-14 11:43:42 +0100

    removed past speaker-events

--- src/components/data/speakers.tsx
+++ src/components/data/speakers.tsx
@@ -10,14 +10,6 @@ class Speaker {
 // modify HERE to update speakers
 
 const speakersRaw = [
-    new Speaker(new Date(2024, 9, 22), "The Role of AI in Disinformation Resilience", "Dr. Veronika Solopova", "TU Berlin, Mantis Analytics", "https://www.meetup.com/bliss-speaker-series/events/303354404/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2024, 10, 5), "The AI Scientist: Towards Fully Automated Open-Ended Scientific Discovery", "Robert Lange", "TU Berlin, SakanaAI", "https://www.meetup.com/bliss-speaker-series/events/303354498/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2024, 10, 19), "Planning in the Age of Learning", "Prof. Marc Toussaint", "TU Berlin", "https://www.meetup.com/bliss-speaker-series/events/303354520/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2025, 0, 21), "Learning Physical Laws from Data", "Prof. Sven Wang", "HU Berlin", "https://www.meetup.com/bliss-speaker-series/events/303354565/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2024, 11, 17), "Combining Reinforcement Learning and Generative Models for de novo Drug Design", "Dr. Nima Siboni & Dr. Miguel Arbesü Andrés", "InstaDeep", "https://www.meetup.com/bliss-speaker-series/events/303354547/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2025, 0, 7), "Making AI fit for the Life Sciences: Informing ML and Explaining Uncertainty", "Prof. Katharina Baum", "FU Berlin", "https://www.meetup.com/bliss-speaker-series/events/303354557/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2024, 11, 3), "Sequential and Active Decision Making: Bandit Theory", "Prof. Alexandra Carpentier", "University of Potsdam", "https://www.meetup.com/bliss-speaker-series/events/303354531/?eventOrigin=bliss_website", false),
-    new Speaker(new Date(2025, 1, 4), "The Science of Decision-Making", "Prof. Sanaz Mostaghim", "University of Magdeburg", "https://www.meetup.com/bliss-speaker-series/events/303354572/?eventOrigin=bliss_website", false),
     // === summersemester 2025, waiting for confirmation to have the events replaced with the real data===
     new Speaker(new Date(2025, 4, 22), "to be announced soon", "", "", "", false, true),
     // new Speaker(new Date(2025, 5, 6), "TBD", "Timothée Darcet", "INRIA France, Meta AI", "TBD", false),
