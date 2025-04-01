6621df3ffb72d3a820a3aacbcfb2d6a1c530e121
derjustus <72736286+MassEast@users.noreply.github.com>
2025-04-01 18:55:29 +0200

    minor fixes

--- src/components/data/speakers.tsx
+++ src/components/data/speakers.tsx
@@ -11,13 +11,13 @@ class Speaker {
 
 const speakersRaw = [
     // === summersemester 2025===
-    new Speaker(new Date("2025-04-22"), "Can Compressing Foundation Models be as Easy as Image Compression?", "Martin Genzel", "Merantix Momentum", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121089", false),
-    new Speaker(new Date("2025-05-06"), "CAPI: Cluster and Predict Latent Patches for Improved Masked Image Modeling", "Timothée Darcet", "Meta AI and Inria", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121112", false),
-    new Speaker(new Date("2025-05-20"), "Scalable Emulation of Protein Equilibrium Ensembles with Generative Deep Learning", "Yu Xie", "Microsoft AI4Science", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121117", false),
-    new Speaker(new Date("2025-06-03"), "Watermark Anything with Localized Messages", "Pierre Fernandez", "Meta FAIR", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121121", false),
-    new Speaker(new Date("2025-06-17"), "Radiance Fields are Dead (and why that’s OK)", "Daniel Duckworth", "Google Deepmind", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121124", false),
-    new Speaker(new Date("2025-07-01"), "Progress in AI Safety and Security", "Adel Bibi", "University of Oxford", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121127", false),
-    new Speaker(new Date("2025-07-15"), "Detecting Hallucinations in Large Language Models Using Semantic Entropy", "Jannik Kossen", "Meta FAIR", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121141", false),
+    new Speaker(new Date("2025-04-22"), "Can Compressing Foundation Models be as Easy as Image Compression?", "Dr. Martin Genzel", "Merantix Momentum", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121089", false),
+    new Speaker(new Date("2025-05-06"), "CAPI: Cluster and Predict Latent Patches for Improved Masked Image Modeling", "Timothée Darcet", "Meta AI, Inria", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121112", false),
+    new Speaker(new Date("2025-05-20"), "Scalable Emulation of Protein Equilibrium Ensembles with Generative Deep Learning", "Dr. Yu Xie", "Microsoft AI4Science", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121117", false),
+    new Speaker(new Date("2025-06-03"), "Watermark Anything with Localized Messages", "Dr. Pierre Fernandez", "Meta AI", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121121", false),
+    new Speaker(new Date("2025-06-17"), "Radiance Fields are Dead (and why that’s OK)", "Daniel Duckworth", "Google DeepMind", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121124", false),
+    new Speaker(new Date("2025-07-01"), "Progress in AI Safety and Security", "Dr. Adel Bibi", "University of Oxford", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121127", false),
+    new Speaker(new Date("2025-07-15"), "Detecting Hallucinations in Large Language Models Using Semantic Entropy", "Jannik Kossen", "Meta AI, University of Oxford", "https://www.meetup.com/de-DE/bliss-speaker-series/events/306121141", false),
 ];
 
 
