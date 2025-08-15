0ebc68ce111e4910be243b41dd5c3d341adc6742
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2025-08-15 18:20:45 +0200

    fix link underlines

--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -11,7 +11,7 @@ export default function BigButton({ id, hover, children, className }: Props) {
   return (
     <div
       id={id}
-      className={`tracking-wider text-accent bg-accent-dark px-4 py-2 text-2xl font-medium text-center rounded-lg no-underline nounderline text-nowrap max-w-min ${hover ? "hover:shadow-accent-small transition-shadow" : ""
+      className={`tracking-wider text-accent bg-accent-dark px-4 py-2 text-2xl font-medium text-center rounded-lg no-underline text-nowrap max-w-min ${hover ? "hover:shadow-accent-small transition-shadow" : ""
         } ${className}`}
     >
       {children}
