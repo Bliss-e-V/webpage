348acb7ef84addfb626a7c2123ff26c1041f9365
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2025-11-14 09:18:37 +0100

    Revert "add workshops page (#85)"
    This reverts commit 8e663d8a776c3157961a4d7987bb4c81609b153b.

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,6 @@ export default defineConfig({
         'https://bliss.berlin/',
         'https://bliss.berlin/blissathon',
         'https://bliss.berlin/reading-group',
-        'https://bliss.berlin/workshops',
         'https://bliss.berlin/media'
       ],
       lastmod: new Date(),
